360890a1bba8588f498ed7a0bc515af9cd2bec7f
DISTORTION_WH <glebsaternik@gmail.com>
2025-11-21 00:22:34 +0300

    браузроутер добавил

--- Frontend/front/src/index.tsx
+++ Frontend/front/src/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; 
+import { BrowserRouter } from "react-router-dom"; 
+import "./index.css";
 import App from "./App";
 
 const rootEl = document.getElementById("root");
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
-  root.render(<App />);
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 }
