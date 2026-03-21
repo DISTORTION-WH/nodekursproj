f314dd4a81856ff5ce677a754898c59f2291d789
DISTORTION_WH <glebsaternik@gmail.com>
2026-03-22 02:09:31 +0300

    fix: missing closing div in ProfilePage view tab (syntax error)

--- apps/web/src/pages/ProfilePage.tsx
+++ apps/web/src/pages/ProfilePage.tsx
@@ -292,6 +292,7 @@ export default function ProfilePage() {
                 )}
               </div>
             </div>
+            </div>
 
             {/* Friends */}
             <ProfileFriendList />
